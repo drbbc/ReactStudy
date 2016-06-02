f2d42ea8525091e0332f0096da2abb27fef902fb
drbbc <drbbc@163.com>
2016-06-02 18:03:53 +0800

    6.26.26.26.26.26.2

---
+++ ios/ReactStudy/Properties.h
@@ -0,0 +1,13 @@
+//
+//  Properties.h
+//  ReactStudy
+//
+//  Created by Rongbin on 16/6/1.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Properties : Object
+
+@end
---
+++ ios/ReactStudy/Properties.m
@@ -0,0 +1,13 @@
+//
+//  Properties.m
+//  ReactStudy
+//
+//  Created by Rongbin on 16/6/1.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import "Properties.h"
+
+@implementation Properties
+
+@end
