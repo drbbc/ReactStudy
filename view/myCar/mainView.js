24dc07cd7d232038d4c8aad755bab1498163e509
drbbc <drbbc@163.com>
2016-06-20 18:07:38 +0800

    上次没有提交完的

--- view/myCar/mainView.js
+++ view/myCar/mainView.js
@@ -1,84 +1,294 @@
-import React,{Component} from  'react';
+import React, {
+  Component
+}
+from 'react';
 //爱车主界面
 import {
-  View,Text,StyleSheet,TouchableHighlight,Image
-} from 'react-native';
+  View, Text, StyleSheet, TouchableHighlight, TouchableOpacity, Image, ListView, PickerIOS, Animated
+}
+from 'react-native';
+
+var PickerItemIOS = PickerIOS.Item;
 
 import NavigationBar from 'react-native-navbar';
 import Global from '../tools/Global';
 import Util from '../tools/utils';
 import Swiper from 'react-native-swiper';
 
-import Svg,{
-    Circle,Line
-} from 'react-native-svg';
-//
-export default class MainView extends Component{
+export default class MainView extends Component {
   constructor() {
     super();
     this.state = {
-      pkCode:''
+      pkCode: '',
+      carList: [],
+      vin: '',
+      carName: '爱车',
+      fadeAnim: new Animated.Value(Util.size.height),
+      buttoms: [
+        '控  制', '车  况', '故  障', '充电桩', '维修点', '经销商'
+      ], carInfo: {}
     };
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.state.pkCode = Global.pkCode;
-    console.log('mainView.state:',this.state.pkCode);
+    let parms = {
+      pkCode: this.state.pkCode
+    };
+    Util.post('car/findCarList', parms, true, (list) => {
+      console.log(list);
+      let carList = list.data.carList;
+      if (carList.length > 0) {
+        console.log(carList[0].carName);
+        this.setState({
+          carList: carList,
+          carName: carList[0].carName,
+          vin: carList[0].vinCode
+        });
+        this._loadCarInfo();
+      }
+    });
   }
 
-  _bannerPress(){
+  _loadCarInfo() {
+    let parms = {
+      pkCode: this.state.pkCode,
+      vinCode: this.state.vin
+    };
+    Util.post('car/carInfo', parms, true, (info) => {
+      console.log('carInfo:' + JSON.stringify(info));
+      this.setState({
+        carInfo: info.data
+      });
+    });
+  }
+
+  componentDidMount() {
+
+    // this.setState({
+    //   fadeAnim:Util.size.height
+    // });
+
+    // Animated.timing(          // Uses easing functions
+    //   this.state.fadeAnim,    // The value to drive
+    //   { toValue: Util.size.height },           // Configuration
+    // ).start();
+  }
+
+  _bannerPress() {
 
   }
 
-  render(){
-    let cricleHeight = Math.max(Util.size.width/3,140)-10;
-    console.log(cricleHeight);
-    return(
-      <View style={styles.container}>
-        <View style={styles.navigation}>
+  _pressNavTitle() {
+    // console.log('clicked:' + this.state.fadeAnim);
+    Animated.timing(          // Uses easing functions
+      this.state.fadeAnim,    // The value to drive
+      { toValue: Util.size.height - 250 },           // Configuration
+    ).start();
+  }
+
+  _chanageSelectedCar(carVin, index) {
+    // console.log(carVin);
+    // console.log(JSON.stringify(this.state.carList));
+    this.setState({
+      vin: this.state.carList[index].vinCode,
+      carName: this.state.carList[index].carName,
+    });
+
+    Animated.timing(          // Uses easing functions
+      this.state.fadeAnim,    // The value to drive
+      { toValue: Util.size.height },           // Configuration
+    ).start();
+
+    this._loadCarInfo();
+  }
+
+  _pressCon(index) {
+    console.log('index:' + index);
+  }
+
+  render() {
+    let cricleHeight = Math.max(Util.size.width / 3, 140) - 10;
+
+    return (
+      <View style = {styles.container} >
+        <View style ={styles.navigation} >
+          <TouchableOpacity onPress={() => this._pressNavTitle() } activeOpacity={0.3}>
+            <View style ={styles.navTitle} >
+              <Text style = {styles.navText} >
+                {this.state.carName}</Text><Image style={{ width: 20, height: 20 }} source={require('../images/nav_arrow_bottom.png') }/>
+            </View>
+          </TouchableOpacity>
         </View>
-        <Swiper height={150} showsButtons={false} autoplay={true}
-          activeDot={<View style={{backgroundColor: 'rgba(255,255,255,0.8)', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />}>
-          <TouchableHighlight onPress={()=> this._bannerPress(11)}>
-            <View style={styles.slide}>
-              <Image style={styles.image} source={require('../images/banner@2x.png')}></Image>
 
+        <Swiper
+          height={150}
+          showsButtons ={false}
+          autoplay ={true}
+          activeDot ={
+            < View style = {
+              {
+                backgroundColor: 'rgba(255,255,255,0.8)',
+                width: 8,
+                height: 8,
+                borderRadius: 4,
+                marginLeft: 3,
+                marginRight: 3,
+                marginTop: 3,
+                marginBottom: 3,
+              }
+            }/>}>
+          <TouchableHighlight onPress = {() => this._bannerPress(11) }>
+            <View style = {styles.slide} >
+              <Image style = {styles.image} source = {require('../images/banner@2x.png') } ></Image>
             </View>
           </TouchableHighlight>
-
         </Swiper>
-        <View style={styles.infoView}>
-          <View style={styles.info1}>
-          </View>
-          <View style={styles.info2}>
-            <Svg style={{alignSelf:'center'}} width={cricleHeight} height={cricleHeight}>
-              <Circle
-                    cx={cricleHeight/2}
-                    cy={cricleHeight/2}
-                    r={cricleHeight/2-10}
-                    stroke={Util.mainColor}
-                    strokeWidth="10"
-                    fill="white"
-                />
-            </Svg>
-          </View>
-          <View style={styles.info3}>
-          </View>
+        {this._initCarInfoUI() }
+        <View style={{height:12,backgroundColor:'rgb(230,230,230)'}}></View>
+        <View style={styles.collection}>
+          {this._initCollectionUI() }
         </View>
-        <Text>MainView
-        </Text>
-      </View>
+
+        <Animated.View style={{
+          top: this.state.fadeAnim,
+          backgroundColor: 'rgb(80,120,200)',
+          position: 'absolute',
+          height: 250,
+          left: 0,
+          right: 0,
+          width: Util.size.width
+        }} >
+          <Text style={{ marginTop: 5, fontSize: 20 }}>车辆列表</Text>
+          <PickerIOS
+            selectedValue={this.state.carName}
+            onValueChange={(carMake, index) => this._chanageSelectedCar(carMake, index) }
+            >
+            {Object.keys(this.state.carList).map((carMake, index) => (
+              <PickerItemIOS
+                key={index}
+                value={this.state.carList[carMake].carName}
+                label={this.state.carList[carMake].carName}
+                />
+            )
+            ) }
+          </PickerIOS>
+        </Animated.View >
+      </View >
     );
   }
+
+  _initCarInfoUI() {
+    console.log('carInfo------' + JSON.stringify(this.state.carInfo));
+    let carInfo = this.state.carInfo.carInfo;
+    if (carInfo != null) {
+      return <View style = {styles.infoView} >
+        <View style = {[styles.info1, { justifyContent: 'center' }]} >
+          <View style = {{ marginLeft: 10 }}>
+            <Text style={{ fontSize: 16, color: 'rgb(100,100,100)' }}>预计续航：</Text>
+            <Text style={{ color: Util.mainColor, fontSize: 16 }}>{carInfo.enduranceMile}
+              <Text style={{ color: 'rgb(100,100,100)' }}> KM</Text>
+            </Text>
+
+            <Text style={{ fontSize: 16, color: 'rgb(100,100,100)', marginTop: 20 }}>可用时长：</Text>
+            <Text style={{ color: Util.mainColor, fontSize: 16 }}>{carInfo.enduranceTime}
+            </Text>
+          </View>
+        </View>
+        <View style = {styles.info2} ></View>
+        <View style = {[styles.info3,{ justifyContent: 'center' }]} >
+          <View style = {{ marginLeft: 10 }}>
+            <Text style={{ fontSize: 16, color: 'rgb(100,100,100)' }}>百公里耗电：</Text>
+            <Text style={{ color: Util.mainColor, fontSize: 16 }}>{carInfo.mileQuantity}
+              <Text style={{ color: 'rgb(100,100,100)' }}> 度</Text>
+            </Text>
+
+            <Text style={{ fontSize: 16, color: 'rgb(100,100,100)', marginTop: 20 }}>已行驶里程：</Text>
+            <Text style={{ color: Util.mainColor, fontSize: 16 }}>{carInfo.totalMileage}
+            <Text style={{ color: 'rgb(100,100,100)' }}> KM</Text>
+            </Text>
+          </View>
+        </View>
+      </View>
+    }
+  }
+
+
+  _initCollectionUI() {
+
+    let outComp = [];
+    for (let i = 0; i < this.state.buttoms.length; i++) {
+      let item = this.state.buttoms[i];
+      let s = '';
+      let img = '';
+      switch (i) {
+        case 0:
+          s = [styles.cItemImage, styles.cItemBk0];
+          img = <Image source={require('../images/indexControl@2x.png') }/>
+          break;
+        case 1:
+          s = [styles.cItemImage, styles.cItemBk1];
+          img = <Image source={require('../images/indexCarInfo@2x.png') }/>
+          break;
+        case 2:
+          s = [styles.cItemImage, styles.cItemBk2];
+          img = <Image source={require('../images/indexMessage@2x.png') }/>
+          break;
+        case 3:
+          s = [styles.cItemImage, styles.cItemBk3];
+          img = <Image source={require('../images/indexElec@2x.png') }/>
+          break;
+        case 4:
+          s = [styles.cItemImage, styles.cItemBk4];
+          img = <Image source={require('../images/indexrepairStation@2x.png') }/>
+          break;
+        case 5:
+          s = [styles.cItemImage, styles.cItemBk5];
+          img = <Image source={require('../images/indexDealer@2x.png') }/>
+          break;
+      }
+
+
+      outComp.push(
+        <View style={styles.collectionItem} key={'list'+i}>
+          <TouchableOpacity onPress={() => this._pressCon(i) }>
+            <View style={styles.cItem}>
+              <View style={s}>
+                {img}
+              </View>
+              <Text style={styles.cItemText}>{item}</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+    return outComp;
+  }
+
+
 }
 
+
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1,
   },
-  navigation:{
-    height:64,
-    backgroundColor:Util.mainColor
+  navigation: {
+    height: 64,
+    backgroundColor: Util.mainColor,
+    alignItems: 'center',
+  },
+  navTitle: {
+    marginTop: 20,
+    height: 44,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  navText: {
+    fontSize: 24,
+    color: 'white',
   },
   slide: {
     flex: 1,
@@ -86,31 +296,79 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  slideText:{
-    position:"absolute",
+  slideText: {
+    position: "absolute",
     bottom: 0,
-    paddingTop:5,
-    paddingBottom:5,
-    backgroundColor:"rgba(255,255,255,0.5)",
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: "rgba(255,255,255,0.5)",
     width: Util.size.width,
-    textAlign:"center",
+    textAlign: "center",
     fontSize: 12
   },
-  image:{
+  image: {
     width: Util.size.width,
     flex: 1,
     alignSelf: 'stretch',
   },
-  infoView:{
-    height:140,
-    flexDirection:'row'
-  },info1:{
-    flex:1,
-    backgroundColor:'rgb(123,156,98)',
-  },info2:{
-    flex:1,
-  },info3:{
-    flex:1,
-    backgroundColor:'rgb(59,100,130)',
+  infoView: {
+    height: 140,
+    flexDirection: 'row'
+  },
+  info1: {
+    flex: 1,
+  },
+  info2: {
+    flex: 1,
+  },
+  info3: {
+    flex: 1,
+  },
+  collection: {
+    height: Util.size.width / 3 * 2,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: Util.size.width,
+    borderBottomColor: 'rgb(210,210,210)',
+    borderBottomWidth: 1,
+
+  }, collectionItem: {
+    width: Util.size.width / 3,
+    height: Util.size.width / 3,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderLeftColor: 'rgb(210,210,210)',
+    borderTopColor: 'rgb(210,210,210)',
+    justifyContent: 'center'
+  }, cItem: {
+    alignItems: 'center',
+  }, cItemText: {
+    marginTop: 5,
+    fontSize: 18,
+    color: 'rgb(20,20,20)'
+  }, cItemImage: {
+
+    width: 61,
+    height: 61,
+    borderRadius: 60,
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, cItemBk0: {
+    backgroundColor: Util.mainColor,
+  }
+  , cItemBk1: {
+    backgroundColor: '#00bab6',
+  },
+  cItemBk2: {
+    backgroundColor: '#f1cc52'
+  },
+  cItemBk3: {
+    backgroundColor: '#ffa200'
+  },
+  cItemBk4: {
+    backgroundColor: '#00a2f4'
+  }, cItemBk5: {
+    backgroundColor: '#ff6666'
   }
 });
--- view/regAndLogin/login.js
+++ view/regAndLogin/login.js
@@ -1,6 +1,6 @@
 //登录页
 'use strict';
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import Util from '../tools/utils';
 import Global from '../tools/Global';
 import MainView from '../myCar/mainView';
@@ -10,6 +10,7 @@ import {
   Text,
   StyleSheet,
   Image,
+  ScrollView,
   TouchableHighlight,
   TouchableOpacity,
   TextInput,
@@ -22,42 +23,42 @@ export default class Login extends Component {
     super();
     this.state = {
       savePassword: 1,
-      pkCode:'',
+      pkCode: '',
     }
     // this.render = this.render.bind(this);
     this._onLoginButton = this._onLoginButton.bind(this);
     this._savePassword = this._savePassword.bind(this);
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.state.pkCode = Global.pkCode;
   }
 
-  _onLoginButton(){
-      var parms = {
-        password:'qqqqqq1',
-        deviceId:'24345B0C-E1A6-4286-9831-9C0139FE6CD5',
-        loginType:'0',
-        userName:'15611189213',
-      };
-      console.log('111111->'+this.state.pkCode);
-      var Properties = NativeModules.Properties;
-      Properties.getDeviceId((error,db)=>{
-        //parms.deviceId = db;
-        Util.post('user/login',parms,true,(response)=>{
-          // console.log(JSON.stringify(response));
-          if (response.state==0) {
-            console.log(response.msg);
-          }
-          if (response.state==1) {
-            Global.pkCode = response.data.pkCode;
-            // this.state.pkCode = response.data.pkCode;
-            this.setState({pkCode:response.data.pkCode});
-            // console.log('222222->'+this.state.pkCode);
-            // console.log(response.msg);
-          }
-        });
+  _onLoginButton() {
+    var parms = {
+      password: 'qqqqqq1',
+      deviceId: '24345B0C-E1A6-4286-9831-9C0139FE6CD5',
+      loginType: '0',
+      userName: '15611189213',
+    };
+    console.log('111111->' + this.state.pkCode);
+    var Properties = NativeModules.Properties;
+    Properties.getDeviceId((error, db) => {
+      //parms.deviceId = db;
+      Util.post('user/login', parms, true, (response) => {
+        // console.log(JSON.stringify(response));
+        if (response.state == 0) {
+          console.log(response.msg);
+        }
+        if (response.state == 1) {
+          Global.pkCode = response.data.pkCode;
+          // this.state.pkCode = response.data.pkCode;
+          this.setState({ pkCode: response.data.pkCode });
+          // console.log('222222->'+this.state.pkCode);
+          // console.log(response.msg);
+        }
       });
+    });
 
     //  fetch('http://down.evcoming.com:5141/zdou_app/user/login',{
     //     method:'POST',
@@ -71,126 +72,141 @@ export default class Login extends Component {
     //   });
 
 
+  }
+
+  _goReg() {
+
+  }
+  _forgetPassword() {
+
+  }
+
+  _savePassword() {
+    // console.log('save password clicked'+this.state.savePassword);
+    if (this.state.savePassword === 1) {
+      this.setState({
+        savePassword: 0
+      });
+    } else {
+      this.setState({
+        savePassword: 1
+      });
     }
+  }
 
-    _goReg(){
-
+  render() {
+    // console.log('render run');
+    var remImg = require('image!jizhumima');
+    if (this.state.savePassword === 1) {
+      remImg = require('image!jizhumimaed');
     }
-    _forgetPassword(){
+    let showView;
 
-    }
-
-    _savePassword(){
-      // console.log('save password clicked'+this.state.savePassword);
-      if (this.state.savePassword===1) {
-        this.setState({
-          savePassword:0
-        });
-      }else{
-        this.setState({
-          savePassword:1
-        });
-      }
-    }
-
-    render(){
-      // console.log('render run');
-      var remImg = require('image!jizhumima');
-      if (this.state.savePassword===1) {
-        remImg = require('image!jizhumimaed');
-      }
-      let showView;
-
-      let loginView = <View style={styles.container}>
-        <Image style={styles.logo} source={require("../images/loginlogo.png")}/>
-        <View style={styles.inputView}>
-          <View style={styles.username}>
-            <Image style={styles.inputImg} source={require('../images/denglu-zh.png')}/>
-            <TextInput style={styles.inputText} placeholder="请输入您的手机号或用户名"/>
+    let loginView =
+      <ScrollView
+        keyboardDismissMode='on-drag'
+        alwaysBounceVertical={false}
+        automaticallyAdjustContentInsets={false}
+        contentContainerStyle={styles.scroll}
+        >
+        <View style={styles.container}>
+          <View style={styles.loginView}>
+            <Image style={styles.logo} source={require("../images/loginlogo.png") }/>
           </View>
-          <View style={{flexDirection:'row',alignItems:'center'}}>
-            <Image style={styles.inputImg} source={require('../images/denglu-mima.png')}/>
-            <TextInput secureTextEntry={true} placeholder='请输入密码' style={styles.inputText}/>
+          <View style={styles.inputView}>
+            <View style={styles.username}>
+              <Image style={styles.inputImg} source={require('../images/denglu-zh.png') }/>
+              <TextInput style={styles.inputText} placeholder="请输入您的手机号或用户名"/>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image style={styles.inputImg} source={require('../images/denglu-mima.png') }/>
+              <TextInput secureTextEntry={true} placeholder='请输入密码' style={styles.inputText}/>
+            </View>
           </View>
-        </View>
-        <TouchableOpacity onPress={this._onLoginButton} activeOpacity={0.6}>
-          <View style={styles.loginButton}>
-            <Text style={styles.loginButtonText}>登录</Text>
-          </View>
-        </TouchableOpacity>
-        <View style={styles.remberView}>
-          <TouchableOpacity onPress={this._savePassword}>
-            <Image ref='savePass' style={{width:20,height:20,marginTop:5}} source={remImg}/>
-            <Text style={{color:Util.mainColor,height:30,top:7,left:20,alignSelf:'auto',position:'absolute'}}>记住密码</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this._forgetPassword}>
-          <Text style={{color:Util.mainColor,height:30,top:-18,position:'absolute',left:Util.size.width-20-70,alignSelf:'flex-end'}}>忘记密码？</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={{marginTop:50}}>
-          <TouchableOpacity onPress={this.goReg}>
-            <Text style={{fontSize:20,color:Util.mainColor}}>注册豆藤
-            </Text>
+          <TouchableOpacity onPress={this._onLoginButton} activeOpacity={0.6}>
+            <View style={styles.loginButton}>
+              <Text style={styles.loginButtonText}>登录</Text>
+            </View>
           </TouchableOpacity>
-
+          <View style={styles.remberView}>
+            <TouchableOpacity onPress={this._savePassword}>
+              <Image ref='savePass' style={{ width: 20, height: 20, marginTop: 5 }} source={remImg}/>
+              <Text style={{ color: Util.mainColor, height: 30, top: 7, left: 20, alignSelf: 'auto', position: 'absolute' }}>记住密码</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._forgetPassword}>
+              <Text style={{ color: Util.mainColor, height: 30, top: -18, position: 'absolute', left: Util.size.width - 20 - 70, alignSelf: 'flex-end' }}>忘记密码？</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ marginTop: 50 }}>
+            <TouchableOpacity onPress={this.goReg}>
+              <Text style={{ fontSize: 20, color: Util.mainColor }}>注册豆藤
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{height:40}}>
+          </View>
         </View>
-      </View>;
+      </ScrollView>
+      ;
 
-      // console.log(this.state.pkCode);
+    // console.log(this.state.pkCode);
 
-      if (this.state.pkCode=='' || this.state.pkCode == 'undefined') {
-        showView = loginView;
-      }else {
-        showView = <Tabbar />;
-      }
-      return showView;
+    if (this.state.pkCode == '' || this.state.pkCode == 'undefined') {
+      showView = loginView;
+    } else {
+      showView = <Tabbar />;
     }
+    return showView;
   }
+}
 
 
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center'
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center',
+    height:Util.size.height
   },
-  logo:{
-    width:111,
-    height:136,
-    marginTop:84,
-  },
-  inputView:{
-    marginTop:30,
-    borderWidth:1,
-    height:90,
-    width:Util.size.width-20,
-    borderColor:Util.mainColor,
-    borderRadius:5
-  },username:{
-    height:45,
-    flexDirection:'row',
-    borderBottomWidth:1,
-    borderColor:Util.mainColor,
-    alignItems:'center',
-  },inputImg:{
-    height:21,
-    width:29,
-    marginLeft:8
-  },inputText:{
-    height:45,flex:5,marginLeft:5,color:'gray',fontSize:18,color:Util.mainColor,
-  },loginButton:{
-    width:Util.size.width-20,
-    alignItems:'center',
+  logo: {
+    width: 111,
+    height: 136,
     marginTop:20,
-    height:40,
-    backgroundColor:Util.mainColor,
-    borderRadius:5,
-    justifyContent:'center'
-  },loginButtonText:{
-    color:'white',
-    fontSize:18,
-  },remberView:{
-    height:30,
-    width:Util.size.width-20,
+  },
+  inputView: {
+    marginTop: 30,
+    borderWidth: 1,
+    height: 90,
+    width: Util.size.width - 20,
+    borderColor: Util.mainColor,
+    borderRadius: 5
+  }, username: {
+    height: 45,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: Util.mainColor,
+    alignItems: 'center',
+  }, inputImg: {
+    height: 21,
+    width: 29,
+    marginLeft: 8
+  }, inputText: {
+    height: 45, flex: 5, marginLeft: 5,fontSize: 18, color: Util.mainColor,
+  }, loginButton: {
+    width: Util.size.width - 20,
+    alignItems: 'center',
+    marginTop: 20,
+    height: 40,
+    backgroundColor: Util.mainColor,
+    borderRadius: 5,
+    justifyContent: 'center'
+  }, loginButtonText: {
+    color: 'white',
+    fontSize: 18,
+  }, remberView: {
+    height: 30,
+    width: Util.size.width - 20,
+  }, scroll: {
+    justifyContent:'center',
   }
-
 });
